5949bf4a3960b38956cd48707372d1e26f1dda10
JobbabCoding <120187934+seungjin777@users.noreply.github.com>
2025-06-02 02:48:09 +0900

    del api

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-#pip install python-dotenv
-#pip install langchain-openai
-#pip install streamlit
-
-from dotenv import load_dotenv
-load_dotenv()
 
 from langchain_openai import ChatOpenAI
 chat_model = ChatOpenAI()
-
-#subject = "AI"
-#result = chat_model.invoke(subject + "에 대한 시를 써줘.")
-#print(result.content)
 
 import streamlit as st
 
